bb963219290b97f01c908a246a9d739ee3b54e4d
abdubakr77 <abdubakora1232@gmail.com>
2026-08-25 07:55:18 +0300

    feat(gru): add GRU model architecture

--- src/model_gru.py
+++ src/model_gru.py
@@ -1,1 +1,25 @@
 """Build and compile the GRU model architecture."""
+
+from keras.models import Sequential
+from keras.layers import Dense,GRU,Dropout,Input
+from keras.optimizers import AdamW
+from keras.callbacks import EarlyStopping
+
+def build_gru(timesteps, num_features,dropout=0,lr=0.0005,weight_decay=0.0005,patience=10, load_weights_path=None):
+    model = Sequential([
+        Input((timesteps, num_features)),
+        GRU(128, return_sequences=True),
+        Dropout(dropout),
+        GRU(32),
+        Dropout(dropout),
+        Dense(1, activation='linear')
+    ])
+
+    optim = AdamW(learning_rate=lr, weight_decay=weight_decay)
+    model.compile(optimizer=optim, loss='mse', metrics=['mse'])
+    model.summary()
+    callbacks = EarlyStopping('val_loss', patience=patience, restore_best_weights=True)
+
+    if load_weights_path: model.load_weights(load_weights_path)
+
+    return model, callbacks
