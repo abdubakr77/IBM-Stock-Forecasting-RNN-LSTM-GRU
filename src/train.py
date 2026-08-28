1e6cebc56aa21aa3a0ab87c18b658970ba88c3d8
abdubakr77 <abdubakora1232@gmail.com>
2026-08-28 08:38:11 +0300

    refactor(training): simplify single-model training workflow

--- src/train.py
+++ src/train.py
@@ -3,32 +3,27 @@
 import os
 import pickle
 
-def train_model(models:list, train_list, valid_list, epochs, batch_size, callbacks, save_dir=os.getcwd()):
-
-    all_trained_models = {}
-    all_models_history = {}
-
-    for model in models:
-
+def train_model(model, train_data, valid_data, epochs, batch_size, callbacks=None, model_name=None, save_dir=os.getcwd()):
+    if model_name is None:
         model_name = model.layers[0].name.upper()
 
-        print(f"{model_name} Is Training...")
+    print(f"{model_name} Is Training...")
 
-        history = model.fit(train_list[0],train_list[1],
-                            validation_data=(valid_list[0],valid_list[1]),
-                            batch_size=batch_size,
-                            epochs=epochs,
-                            verbose=1,
-                            callbacks=[callbacks],
-                            shuffle=False)
+    history = model.fit(
+        train_data[0], train_data[1],
+        validation_data=(valid_data[0], valid_data[1]),
+        batch_size=batch_size,
+        epochs=epochs,
+        verbose=1,
+        callbacks=callbacks if isinstance(callbacks, list) else [callbacks] if callbacks else None,
+        shuffle=False
+    )
 
-        model.save(os.path.join(save_dir,f"trained_model_{model_name}.keras"),overwrite=True)
-        with open(os.path.join(save_dir,f'{model_name}_history.pkl'), 'wb') as f:
-            pickle.dump(history, f)
+    model.save(os.path.join(save_dir, f"trained_model_{model_name}.keras"), overwrite=True)
+    
+    with open(os.path.join(save_dir, f"{model_name}_history.pkl"), "wb") as f:
+        pickle.dump(history, f)
 
-        all_trained_models[model_name] = model
-        all_models_history[f"{model_name}_history"] = history
-        
-        print(f'\n{model_name} Saved Successfully!')
+    print(f"\n{model_name} Saved Successfully!")
 
-    return all_trained_models , all_models_history
+    return model, history
